9b205f21aea89b0c385187a3d9d59000e40cf7bb
Ally Netzel <netzelally@gmail.com>
2018-12-21 20:41:31 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,5 +5,14 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
+    begin
+     @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
+          req.params['client_id'] ='e3590f5e7c619934ca0b'
+          req.params['client_secret'] = '5cd4bae7ec5ad7432004eace3bcbf581eb2849b9'
+          req.params['q'] = '20160201'
+          req.params['near'] = params[:zipcode]
+          req.params['query'] = 'coffee shop'
+          #req.options.timeout = 0
+        end
   end
 end
